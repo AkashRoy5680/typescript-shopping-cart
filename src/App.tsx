1fb1cb7ad71b516bd4ee62da54b7d77c1c414615
AkashRoy5680 <royakash1272@gmail.com>
2022-09-11 05:17:11 +0200

    product added

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import {useState} from "react"
 import {useQuery} from "react-query"
+import Item from './Item/Item';
 
 //Components
 import Drawer from "@material-ui/core/Drawer";
@@ -8,8 +9,9 @@ import Grid from "@material-ui/core/Grid";
 import AddShoppingCartIcon from "@material-ui/icons/AddShoppingCart";
 import Badge from "@material-ui/core/Badge";
 
+
 //styles
-import { wrapper } from "./App.styles";
+import { Wrapper } from "./App.styles";
 
 //Types
 export type CartItemType={
@@ -33,16 +35,22 @@ const App=() => {
   );
     console.log(data);
     const getTotalItems =() =>null;
-    const handleAddToCart =() =>null;
+    const handleAddToCart =(clickedItem: CartItemType) =>null;
     const handleRemoveFromCart =() =>null;
 
     if(isLoading) return <LinearProgress/>
     if(error) return <div>Something went wrong...</div>
 
   return (
-    <div className="App">
-     start
-    </div>
+   <Wrapper>
+    <Grid container spacing={3}>
+      {data?.map(item=>(
+        <Grid item key={item.id} xs={12} sm={4}>
+          <Item item={item} handleAddToCart={handleAddToCart} />
+        </Grid>
+      ))}
+    </Grid>
+   </Wrapper>
   );
 }
 
---
+++ src/Item/Item.tsx
@@ -0,0 +1,26 @@
+import Button from "@material-ui/core/Button";
+import { Wrapper } from "./Item.styles";
+
+//Types
+import { CartItemType } from "../App";
+//Styles
+
+type Props={
+    item:CartItemType;
+    handleAddToCart: (clickedItem: CartItemType) =>void;
+}
+
+const Item: React.FC<Props> =({item, handleAddToCart}) => (
+    <Wrapper>
+        <img src={item.image} alt={item.title} />
+        <div>
+            <h3>{item.title}</h3>
+            <p>{item.description}</p>
+            <h3>{item.price}</h3>
+        </div>
+        <button onClick={()=>handleAddToCart(item)}>Add to Cart</button>
+    </Wrapper>
+);
+
+export default Item;
+
